6952727d1cea145857afeb109399961bce0a341d
UmeHabiba2416 <umehabiba1624@gmail.com>
2026-01-20 18:27:57 +0500

    Add files via upload

---
+++ creative site/script.js
@@ -0,0 +1,11 @@
+function goToServices() {
+    window.location.href = "services.html";
+}
+
+function goToHome() {
+    window.location.href = "index.html";
+}
+
+function showServiceMessage() {
+    alert("Thank you! Our team will contact you soon.");
+}
